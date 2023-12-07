eb337fa541ea0e45c2b453c357cd9a226b935923
BarJaw <83785015+BarJaw@users.noreply.github.com>
2023-12-07 18:44:56 +0100

    Added orders table

--- create_tables.sql
+++ create_tables.sql
@@ -1,5 +1,6 @@
 -- ATTACH DATABASE './Bookstore.db' as Bookstore;
 DROP TABLE IF EXISTS Users;
+DROP TABLE IF EXISTS Orders;
 
 CREATE TABLE Users (
     id INTEGER PRIMARY KEY AUTOINCREMENT,
@@ -10,3 +11,23 @@ CREATE TABLE Users (
     salt TEXT NOT NULL,
     role TEXT NOT NULL
 );
+
+CREATE TABLE Orders (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    date TEXT NOT NULL,
+    priority TEXT NOT NULL,
+    status TEXT NOT NULL,
+    address TEXT NOT NULL,
+    book_list TEXT NOT NULL,
+    user_id INTEGER NOT NULL,
+    FOREIGN KEY (user_id) REFERENCES Users(id)
+);
+
+
+-- INSERT INTO Users (fname, lname, login, password, salt, role)
+-- VALUES
+-- ('b', 'j', 'bj', 'pass', '1', 'admin');
+
+-- INSERT INTO Orders (date, priority, status, address, book_list, user_id)
+-- VALUES
+-- ('07.12.2023', 'high', 'in progress', 'essa 23/1', '{"dziady": 1}', 1);
