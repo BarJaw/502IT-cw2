bd93c2a9240045a80c3c8f61395623ae22c0ddf7
BarJaw <83785015+BarJaw@users.noreply.github.com>
2023-12-09 11:40:41 +0100

    imported classes, created main function

--- main.py
+++ main.py
@@ -1,27 +1,35 @@
+from customer import Customer
+from order import Order
+from book import Book
+from user import User
+from colors import blue_text, green_text, red_text
+
+import sqlite3
+import bcrypt
+
+
 class System():
     @staticmethod
     def main():
         while True:
-            System.view_menu()
-    
+            System.view_initial_menu()
+
     @staticmethod
-    def view_menu():
-        print("\n1. Register\n2. Login\n3. Exit")
-        choice = input("\033[94mChoose an option: \033[0m")
+    def view_initial_menu():
+        print("""\n1. Register\n2. Login\n3. Exit""")
+        choice = input(blue_text("Choose an option: "))
 
         if choice == "1":
-            auth_system.register()
+            return User.login('Bartosz', 'Jaworski')
+            ... # login
         elif choice == "2":
-            auth_system.login()
+            return Customer.register()
+            ...
         elif choice == "3":
             exit(1)
         else:
-            print("\033[91m=== Invalid choice. Please choose again\033[0m")
-
-    
+            print(red_text("Invalid choice. Please choose again"))
 
 
 if __name__ == '__main__':
     System.main()
-    
-    
