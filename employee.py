ef428a3c2bf5c5fbced60c00b9273a4755a33f1e
BarJaw <83785015+BarJaw@users.noreply.github.com>
2023-12-10 21:07:09 +0100

    Adding book feature

--- employee.py
+++ employee.py
@@ -5,6 +5,7 @@ from book import Book
 from colors import red_text, green_text, blue_text
 from getpass import getpass
 from prettytable import PrettyTable
+from utils import is_float
 
 
 class Employee(User):
@@ -16,7 +17,8 @@ class Employee(User):
         con = sqlite3.connect("db/Bookstore.db")  # connect to db
         con.row_factory = sqlite3.Row
         cur = con.cursor()
-        cur.execute(f"SELECT * FROM Books ORDER BY RANDOM();") # SELECT RANDOM SO IT CAN BE SORTED LATER!!!!
+        # SELECT RANDOM SO IT CAN BE SORTED LATER!!!!
+        cur.execute(f"SELECT * FROM Books ORDER BY RANDOM();")
 
         # Get column names
         column_names = [description[0] for description in cur.description]
@@ -30,24 +32,54 @@ class Employee(User):
 
     @staticmethod
     def add_book():
-        # INSERT INTO Books (name, author, stock, price) VALUES ('dziady cz. 1', 'mickiewicz', 10, 5.99);
+        con = sqlite3.connect("db/Bookstore.db")
+        con.row_factory = sqlite3.Row
+        cur = con.cursor()
+
         try:
             print('Please provide the following book details.')
-            book_name = input('Book title: ')
-            author = input('Author: ')
-            quantity = int(input('Stock: '))
-            price = float(input('Price: '))
-            con = sqlite3.connect("db/Bookstore.db")  # connect to db
-            con.row_factory = sqlite3.Row
-            cur = con.cursor()
-            cur.execute("INSERT INTO Books (name, author, stock, price) VALUES (?,?,?,?);",
-                        (book_name, author, quantity, price))
-            con.commit()
-            con.close()
-            print(green_text('Book successfully added'))
-        except:
-            print(red_text('Something went wrong. Please try again'))
-        
+
+            book_name = input('Book title: ').strip()
+            while not book_name:
+                print(red_text(
+                    'Book title you provided is incorrect. Please provide correct book title'))
+                book_name = input('Book title: ').strip()
+
+            # This if checks whether the book already exists in the db. If not then inserts the book, else throws an error.
+            if cur.execute("SELECT name FROM Books WHERE name = (?)", (book_name,)).fetchone() is not None:
+                author = input('Author: ')
+                while not author:
+                    print(
+                        red_text('Author you provided is incorrect. Please provide correct author.'))
+                    author = input('Author: ')
+
+                quantity = input('Stock: ')
+                while not quantity.isdecimal() or int(quantity) < 0:
+                    print(red_text(
+                        'Quantity you provided is incorrect. Please provide correct quantity.'))
+                    quantity = input('Stock: ')
+                quantity = int(quantity)
+
+                price = input('Price: ')
+                while not price or not is_float(price) or float(price) < 0:
+                    print(red_text(
+                        'Price you provided is incorrect. Please provide correct price.'
+                    ))
+                    price = input('Price: ')
+                price = float(price)
+
+                cur.execute("INSERT INTO Books (name, author, stock, price) VALUES (?,?,?,?);",
+                            (book_name, author, quantity, price))
+                con.commit()
+                con.close()
+                print(green_text('Book successfully added'))
+            else:
+                print(
+                    red_text('Such book already exists. Please edit the existing position.'))
+        except Exception as e:
+            print(red_text('Something went wrong. Please try again.'))
+            print(e)
+
     def remove_book(self, book_id):
         # Add code to remove a book from the database
         # You can use the book's ID to identify and delete the book
