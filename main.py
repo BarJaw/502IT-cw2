c9b518541af0270a8d34f1642b7118ba43311913
BarJaw <83785015+BarJaw@users.noreply.github.com>
2023-12-10 14:00:00 +0100

    get_credentials

--- main.py
+++ main.py
@@ -15,14 +15,21 @@ class System():
             System.view_initial_menu()
 
     @staticmethod
+    def get_credentials() -> dict:
+        creds = {
+            'username': input('Username: ').encode('utf-8'),
+            'password': input('Password: ').encode('utf-8')
+        }
+        return creds
+    
+    @staticmethod
     def view_initial_menu():
         print("\n1. Login\n2. Register\n3. Exit")
         choice = input(blue_text("Choose an option: "))
         match choice:
             case "1":  # Logging in process
-                username = input("Username: ").encode('utf-8')
-                password = input("Password: ").encode('utf-8')
-                User.login(username, password)
+                credentials = System.get_credentials()
+                User.login(credentials)
             case "2":
                 ...
                 return Customer.register()
@@ -31,6 +38,6 @@ class System():
             case _:
                 print(red_text("Invalid choice. Please choose again"))
 
-
+    
 if __name__ == '__main__':
     System.main()
