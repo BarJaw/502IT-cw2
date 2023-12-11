b5f9a4efaaaf3654cc058ea3a4abe46b5a82302c
Hanmin Lee <mongmanny04@gmail.com>
2023-12-11 22:49:25 +0100

    Updated Book class

--- order.py
+++ order.py
@@ -8,6 +8,7 @@ class Order:
                  priority: str,
                  status: str,
                  address: str,
+                 estimated_date_of_arrival: str,
                  book_list: dict,
                  user_id: int
                  ):
@@ -15,18 +16,7 @@ class Order:
         self.date = order_date
         self.priority = priority
         self.status = status
-        self.address = address
-        # Assuming book_list is a dictionary {book_id: quantity}
+        self.address = address    
+        self.estimated_date_of_arrival = estimated_date_of_arrival
         self.book_list = book_list
         self.user_id = user_id
-
-    def display_order_details(self):
-        print(f"Order ID: {self.id}")
-        print(f"Order Date: {self.date}")
-        print(f"Priority: {self.priority}")
-        print(f"Status: {self.status}")
-        print(f"Address: {self.address}")
-        print("Book List:")
-        for book_id, quantity in self.book_list.items():
-            print(f"  Book ID: {book_id}, Quantity: {quantity}")
-        print(f"User ID: {self.user_id}")
