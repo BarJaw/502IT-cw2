3546f7c59ddfbba0c9fc8a9dd46a2a2e44e19623
Hanmin Lee <mongmanny04@gmail.com>
2023-12-12 09:45:23 +0100

    Updated Book class

--- book.py
+++ book.py
@@ -8,10 +8,3 @@ class Book:
 
     def edit_quantity(self, new_quantity):
         self.stock = new_quantity
-
-    def display_book_details(self):
-        print(f"Book ID: {self.id}")
-        print(f"Name: {self.name}")
-        print(f"Author: {self.author}")
-        print(f"Stock: {self.stock}")
-        print(f"Price: ${self.price:.2f}")
